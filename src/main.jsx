699aec85302eb829204916c02da7bc75a5fb07fd
NaufalFerdiansyahPutra <naufalperdiansyah@gmail.com>
2026-04-03 00:00:58 +0700

    tugas3

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import TugasPert3 from './tugaspert3/FormBudget.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById('root'))
+const pathname = window.location.pathname
+
+const element = pathname === '/tugaspert3' || pathname === '/tugaspert3/'
+  ? (
+    <StrictMode>
+      <TugasPert3 />
+    </StrictMode>
+  ) : (
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+
+root.render(element)
