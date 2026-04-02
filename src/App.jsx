02580af1917fef03faeec90d803d9422e75cecb1
defani24si <defani24si@mahasiswa.pcr.ac.id>
2026-04-02 21:21:28 +0700

    latihanp3

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import BiodataDiri from './latihanp2/BiodataDiri'
+import FormBudget from './latihanp3/FormBudget'
+
 import './App.css'
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './latihanp3/tailwind.css'
+import FormBudget from './latihanp3/FormBudget'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <FormBudget/>
   </StrictMode>,
 )
