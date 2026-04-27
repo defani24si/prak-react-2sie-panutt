21bd4bf8053a20bfd76b188d1f7fecf1480db103
defani24sie <defani24si@mahasiswa.pcr.ac.id>
2026-04-27 16:30:25 +0700

    pertemuan7

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import Sidebar from './pertemuan-5/layouts/Sidebar.jsx'
-import './pertemuan-5/assets/tailwind.css'
+import Sidebar from './components/Sidebar.jsx'
+import './assets/tailwind.css'
+import Header from './components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <div className="bg-[#f3f4f6] min-h-screen flex font-poppins text-gray-800">
-        <Sidebar />
+        {/* <Sidebar /> */}
         <div className="flex-1 p-4 overflow-y-auto">
           <App />
         </div>
